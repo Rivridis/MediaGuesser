20477b1f6fae6aba312e8bc5cf046be3cff9fca3
Rivridis <sanjaypugal.personal@gmail.com>
2025-03-04 21:24:54 +0530

    guess bar change

--- src/app/components/guessbar.jsx
+++ src/app/components/guessbar.jsx
@@ -2,15 +2,18 @@
 import React from "react";
 
 const GuessBar = ({ gcount }) => {
-    console.log(gcount);
-    return (
-        <div className = "flex flex-row">
-            {[...Array(gcount)].map((_, index) => (
-            <div key={index} className="w-8 h-8 mx-2 bg-red-400 rounded-lg"></div>
-          ))}
-        </div>
-    );
-  };
-  
-  export default GuessBar;
-  
+  return (
+    <div className="flex flex-row">
+      {[...Array(5)].map((_, index) => (
+        <div
+          key={index}
+          className={`w-8 h-8 mx-2 rounded-lg ${
+            index < gcount ? "bg-red-400" : "bg-gray-400"
+          }`}
+        ></div>
+      ))}
+    </div>
+  );
+};
+
+export default GuessBar;
